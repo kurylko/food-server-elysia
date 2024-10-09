9a7b4612c5347768c54455f516dd178d82ec3c83
kurylko <reviliya@gmail.com>
2024-10-09 21:58:13 +0200

    routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Food-server")
     .get("/post/:id", ({params: {id}}) => {return{id: id, title: "Bun"}})
+    .post("/post", (context) => {return context})
+    .get("/track/*/", () => {return "Track route"})
     .listen(5000);
 
 console.log(
